d2f72e2a2a0b667726a9012fd244c28daba0840c
yuzhifei <kingdomrain002@163.com>
2020-07-05 21:50:45 +0800

    sonar

---
+++ DevOpsDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DevOpsDemo
+//
+//  Created by kingdomrain on 2020/7/4.
+//  Copyright © 2020 kingdomrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        NSLog("hello world");
+    }
+
+
+}
+
